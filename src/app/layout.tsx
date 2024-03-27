ef22fcc9b88d60ea24cef0107c8f7fe8c2850bcd
soyeon <dev.vsoyeon@gmail.com>
2024-03-27 10:21:20 +0900

    네이버 서치 어드바이저(2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { GoogleAnalytics } from '@next/third-parties/google';
+// import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 export const metadata: Metadata = {
