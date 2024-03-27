cf501bc5a6ec3e69d401bedd66b482d2e5611f89
soyeon <dev.vsoyeon@gmail.com>
2024-03-27 11:10:02 +0900

    구글 서치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-// import { GoogleAnalytics } from '@next/third-parties/google';
-import type { Metadata } from 'next';
-import '@/styles/globals.css';
+// import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from "next";
+import "@/styles/globals.css";
 export const metadata: Metadata = {
-  title: '전국 보건소 위치 안내',
-  description: '전국 보건소 위치 안내 서비스 앱',
+  title: "전국 보건소 위치 안내",
+  description: "전국 보건소 위치 안내 서비스 앱",
 };
 
 export default function RootLayout({
@@ -14,17 +14,20 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-      <meta name="naver-site-verification" content="34354046817ce9ba08c0873250fe14840eb042f2" />
+        <meta
+          name="naver-site-verification"
+          content="34354046817ce9ba08c0873250fe14840eb042f2"
+        />
         <link
           rel="canonical"
           href="https://health-center-sigma.vercel.app"
         ></link>
 
-        {/* <meta
+        <meta
           name="google-site-verification"
-          content="Q2bHOLs6jaFYy4NK_gCCdUCjf270wQ8zYE_nerDd7lw"
+          content="CjhiNpJOA2vVV19-D1aG2TlXazXl8oGe61gDC2Tchn0"
         />
-        <GoogleAnalytics gaId="G-L57MXLTT6K" /> */}
+        {/* <GoogleAnalytics gaId="G-L57MXLTT6K" /> */}
       </head>
       <body>{children}</body>
     </html>
